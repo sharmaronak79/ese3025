99b9e5694e01f0bec01c5b90b98abe3a33c4206a
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-03-14 22:37:49 -0400

    Create mute.c

---
+++ thread/mute.c
@@ -0,0 +1,50 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+pthread_mutex_t lock;
+int j;
+
+void
+do_process()
+{
+    pthread_mutex_lock(&lock);
+    int i = 0;
+
+    j++;
+
+    while(i < 5)
+    {
+        printf("%d", j);
+        sleep(1);
+
+        i++;
+    }
+
+    printf("...Done\n");
+
+    pthread_mutex_unlock(&lock);
+}
+
+int
+main(void)
+{
+    int err;
+    pthread_t t1, t2;
+
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("Mutex initialization failed.\n");
+        return 1;
+    }
+
+    j = 0;
+
+    pthread_create(&t1, NULL, do_process, NULL);
+    pthread_create(&t2, NULL, do_process, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    return 0;
+}
