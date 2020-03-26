8a1da76a4581fbd7a8a212e16fa00a2b083d4d96
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2020-03-26 19:12:57 -0400

    Create onBoardButton_LED_blink.c

---
+++ LPC/LED/onBoardButton_LED_blink.c
@@ -0,0 +1,42 @@
+#include "board.h"
+
+/* Sets up system hardware */
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Initial LEDs state is off */
+	Board_LED_Set(0, true);  //RED   LED OFF
+	Board_LED_Set(1, true);  //GREEN LED OFF
+	Board_LED_Set(2, true);  //BLUE  LED OFF
+
+}
+
+int main()  {
+
+	prvSetupHardware();
+	int i=0;
+
+	while(1) {
+	bool button_state = false ;
+	bool LedState = false;
+
+	if(Buttons_GetStatus()==0x01) {
+	  button_state=true ;
+	}
+
+	    if(button_state){
+
+			Board_LED_Set(i, LedState); }
+			else {
+			LedState = (bool) !LedState;
+			Board_LED_Set(i, LedState);
+			if(i != 2) { i++ ; }
+			else { i=0 ; }
+
+			   			       }
+	    }
+
+	return 0 ;
+}
